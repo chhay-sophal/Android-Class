d7238cdc52067495275efab406a4107e0d8ac46a
Sophal Chhay <134204301+chhay-sophal@users.noreply.github.com>
2024-05-14 18:47:14 +0700

    Assignment: remove movie by clicking x button

--- app/src/main/java/com/example/myapplication/state_movie_module/screens/state_movie_screen.kt
+++ app/src/main/java/com/example/myapplication/state_movie_module/screens/state_movie_screen.kt
@@ -1,18 +1,19 @@
 package com.example.myapplication.state_movie_module.screens
 
-import android.graphics.drawable.Icon
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
@@ -23,7 +24,6 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import coil.compose.AsyncImage
-import com.example.myapplication.state_movie_module.models.stateMovieList1
 import com.example.myapplication.state_movie_module.viewmodels.StateMovieViewModel
 
 @Composable
@@ -31,33 +31,49 @@ fun StateMovieScreen(navController: NavController, movieVM: StateMovieViewModel 
     Box(
         modifier = Modifier.fillMaxSize()
     ) {
-        LazyColumn() {
-            items(stateMovieList1.size) { index ->
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Absolute.SpaceBetween
-                ) {
-                    Text(
-                        stateMovieList1[index].name,
-                        modifier = Modifier.padding(10.dp)
-                    )
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(Icons.Default.Close, contentDescription = null)
+        Column {
+            Row {
+                Spacer(modifier = Modifier.weight(1f))
+                Button(onClick = {
+                    movieVM.sortMoviesByTitle()
+                }) {
+                    if (!movieVM.sortedAZ.value) {
+                        Text(text = "Sort By Title Ascending")
+                    } else {
+                        Text(text = "Sort By Title Descending")
                     }
                 }
+            }
+            LazyColumn() {
+                items(movieVM.movieList.value.size) { index ->
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Absolute.SpaceBetween
+                    ) {
+                        Text(
+                            movieVM.movieList.value[index].name,
+                            modifier = Modifier.padding(10.dp)
+                        )
+                        IconButton(onClick = {
+                            movieVM.removeMovie(index)
+                        }) {
+                            Icon(Icons.Default.Close, contentDescription = null)
+                        }
+                    }
 
-                AsyncImage(
-                    modifier = Modifier
-                        .size(width = 500.dp, height = 300.dp)
-                        .fillParentMaxWidth()
-                        .clickable {
-                            movieVM.selectedMovie.value = stateMovieList1[index]
-                            navController.navigate("detail")
-                        },
-                    model = stateMovieList1[index].image,
-                    contentDescription = null,
-                    contentScale = ContentScale.Crop
-                )
+                    AsyncImage(
+                        modifier = Modifier
+                            .size(width = 500.dp, height = 300.dp)
+                            .fillParentMaxWidth()
+                            .clickable {
+                                movieVM.selectedMovie.value = movieVM.movieList.value[index]
+                                navController.navigate("detail")
+                            },
+                        model = movieVM.movieList.value[index].image,
+                        contentDescription = null,
+                        contentScale = ContentScale.Crop
+                    )
+                }
             }
         }
     }
--- app/src/main/java/com/example/myapplication/state_movie_module/viewmodels/StateMovieViewModel.kt
+++ app/src/main/java/com/example/myapplication/state_movie_module/viewmodels/StateMovieViewModel.kt
@@ -8,16 +8,24 @@ import com.example.myapplication.state_movie_module.models.stateMovieList1
 
 class StateMovieViewModel: ViewModel() {
 //    var movieList: MutableList<MovieModel> = movieList1.toMutableList()
-    val movieList: MutableState<List<StateMovieModel>> = mutableStateOf(stateMovieList1)
+val movieList: MutableState<List<StateMovieModel>> = mutableStateOf(stateMovieList1)
     val selectedMovie: MutableState<StateMovieModel> = mutableStateOf(StateMovieModel())
-    private var sortedAZ: MutableState<Boolean> = mutableStateOf(false)
+    var sortedAZ: MutableState<Boolean> = mutableStateOf(false)
 
-    fun SortMoviesByTitle() {
+    fun sortMoviesByTitle() {
         sortedAZ.value = !sortedAZ.value
         if (sortedAZ.value) {
             movieList.value = movieList.value.sortedBy { it.name }
         } else {
             movieList.value = movieList.value.sortedByDescending { it.name }
+        }
+    }
+
+    fun removeMovie(index: Int) {
+        if (index >= 0 && index < movieList.value.size) {
+            val newMutableList = movieList.value.toMutableList()
+            newMutableList.removeAt(index)
+            movieList.value = newMutableList
         }
     }
 }
