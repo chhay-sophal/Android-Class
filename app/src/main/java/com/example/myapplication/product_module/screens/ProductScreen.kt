65a622630503eaa2754976537856a39a2ed02abe
Sophal Chhay <134204301+chhay-sophal@users.noreply.github.com>
2024-08-15 13:46:33 +0700

    display product images

--- app/src/main/java/com/example/myapplication/product_module/screens/ProductScreen.kt
+++ app/src/main/java/com/example/myapplication/product_module/screens/ProductScreen.kt
@@ -1,13 +1,23 @@
 package com.example.myapplication.product_module.screens
 
+import android.util.Log
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -15,8 +25,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.example.myapplication.product_module.models.Product
 import com.example.myapplication.product_module.viewmodels.ProductViewModel
 
@@ -39,7 +51,9 @@ fun ProductScreen(productViewModel: ProductViewModel) {
         }
     ) {
         Box(
-            modifier = Modifier.padding(it).fillMaxSize(),
+            modifier = Modifier
+                .padding(it)
+                .fillMaxSize(),
             contentAlignment = Alignment.Center
         ) {
             when {
@@ -71,15 +85,35 @@ fun ProductList(products: List<Product>) {
 
 @Composable
 fun ProductItem(product: Product) {
+    Log.d("ProductItem", "Image URL: ${product.image}")
+
     Box(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(8.dp)
-            .background(Color.LightGray)
+            .padding(8.dp, 5.dp)
+            .background(Color.LightGray, RoundedCornerShape(10.dp))
+            .border(BorderStroke(1.dp, Color.DarkGray), RoundedCornerShape(10.dp))
     ) {
-        Text(
-            text = "${product.title} - ${product.price}",
-            modifier = Modifier.padding(16.dp)
-        )
+        Row(modifier = Modifier.padding(10.dp)) {
+            AsyncImage(
+                model = product.image,
+                contentDescription = null,
+                modifier = Modifier
+                        .size(100.dp)
+                        .clip(RoundedCornerShape(8.dp))
+                        .background(Color.Gray)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            Column {
+                Text(
+                    text = product.title,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+                Text(
+                    text = product.price,
+                    style = MaterialTheme.typography.bodyLarge
+                )
+            }
+        }
     }
 }
